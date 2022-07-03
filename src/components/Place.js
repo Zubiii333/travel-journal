41b05b946ce0439d666565e00229218f69d5e930
zougari47 <zougariahmed007@gmail.com>
2022-07-03 18:16:13 +0100

    fix img not shown

--- src/components/Place.js
+++ src/components/Place.js
@@ -1,16 +1,25 @@
-import React from 'react'
+import React from 'react';
 
 function Place(props) {
-  // const coverImg = '../images/Amsterdam.jpg';
+  const coverImg = require(`../images/${props.coverImg}`);
   return (
-  <div style={{ backgroundImage: '../images/Amsterdam.jpg' }} className='my-component-class'>
-        <div className='block'>
+    <div
+      style={{
+        backgroundImage: `url(${coverImg})`,
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        border: '1px solid black'
+      }}
+      className="my-component-class"
+    >
+      <div className="block">
         <h1 className="title">{props.title}</h1>
-        <h2 className='country'>{props.country}</h2>
-        </div>
-        <h1 className='date'>{props.date}</h1>
+        <h2 className="country">{props.country}</h2>
+      </div>
+      <h1 className="date">{props.date}</h1>
     </div>
-  )
+  );
 }
 
-export default Place
+export default Place;
